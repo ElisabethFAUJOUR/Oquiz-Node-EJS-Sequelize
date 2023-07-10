df42c1df0af9593e869de36add41396badeedfde
Elisabeth Faujour <efaujour@gmail.com>
2023-07-10 09:52:27 +0200

    etape 1

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,18 @@
+const { Quiz } = require("../models/index.js");
+
 const mainController = {
-  renderHomePage(req, res) {
-    res.render("home");
+  async renderHomePage(req, res) {
+    try {
+      const quizzes = await Quiz.findAll({
+        include : [
+          "author",
+          "tags"
+        ]
+      });
+      res.render("home", { quizzes });
+    } catch(error) {
+      res.render("home", { error });
+    }
   }
 };
 
